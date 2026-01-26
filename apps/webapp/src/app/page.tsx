31b453b14b9a7b1202e93a74bd5c236ab69b502f
Jun Ito <jujunjun110@gmail.com>
2026-01-27 03:12:06 +0900

    Update: dynamic top

--- apps/webapp/src/app/page.tsx
+++ apps/webapp/src/app/page.tsx
@@ -5,6 +5,8 @@ import { loadVideos } from '@/server/presentation/loaders/loadVideos';
 import { Plus } from 'lucide-react';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const response = await loadVideos();
 
